fixed homotopy computation

mpp/robot/displayXspacePCA.py
@@ -1,41 +1,38 @@
+import sys,os
+sys.path.append(os.environ["MPP_PATH"]+"mpp-robot/mpp")
+
 import numpy as np
 from pylab import *
 from mpl_toolkits.mplot3d import axes3d
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+from robot.plot_trisurf_fixed import *
 
-def getMainPCAaxes(Xarray,h3,Npts):
+def getMainPCAaxes(Xarray):
+        N = len(Xarray[0])
         if len(Xarray)>0:
                 ## Xarray to MxN numpy array
-                xx = np.zeros((Npts,len(Xarray)))
+                xx = np.zeros((N,len(Xarray)))
                 for i in range(0,len(Xarray)):
-                        for j in range(0,Npts):
+                        for j in range(0,N):
                                 xx[j,i] = Xarray[i][j]
-
-                #for i in range(0,Npts):
-                #        xd = 0
-                #        for j in range(0,len(Xarray)):
-                #                xd = xd+xx[i,j]
-                #        xd = xd/len(Xarray)
-                #        for j in range(0,len(Xarray)):
-                #                xx[i,j]=xx[i,j]-xd
 
                 xx = preprocessing.scale(xx)
                 [U,S,V]=np.linalg.svd(xx)
-                print np.around(S,1)
+                #print np.around(S,1)
                 uu = np.around(U,2)
 
                 X1 = uu[:,0]
                 X2 = uu[:,1]
                 X3 = uu[:,2]
 
-                return [X1,X2,X3]
+                return [X1,X2,X3,np.around(S,1)]
 
-def plotDataMainAxes(Xarray,PCaxes,h3,Npts,fname):
-        if len(Xarray)>Npts:
-                PCaxes = getMainPCAaxes(Xarray,h3,Npts)
-                [X1,X2,X3] = PCaxes
+def projectDataOnto3MainAxes(Xarray):
+                Npts = len(Xarray[0])
+                PCaxes2 = getMainPCAaxes(Xarray)
+                [X1,X2,X3,S] = PCaxes2
 
                 ## Xarray to MxN numpy array
                 xx = np.zeros((Npts,len(Xarray)))
@@ -43,13 +40,39 @@ def plotDataMainAxes(Xarray,PCaxes,h3,Npts,fname):
                         for j in range(0,Npts):
                                 xx[j,i] = Xarray[i][j]
 
-                #for i in range(0,Npts):
-                #        xd = 0
-                #        for j in range(0,len(Xarray)):
-                #                xd = xd+xx[i,j]
-                #        xd = xd/len(Xarray)
-                #        for j in range(0,len(Xarray)):
-                #                xx[i,j]=xx[i,j]-xd
+                xx = preprocessing.scale(xx)
+
+                Xproj = np.zeros((3,len(Xarray)))
+                for i in range(0,len(Xarray)):
+                        x = np.dot(X1.T,xx[:,i])
+                        y = np.dot(X2.T,xx[:,i])
+                        z = np.dot(X3.T,xx[:,i])
+                        Xproj[0,i] = x
+                        Xproj[1,i] = y
+                        Xproj[2,i] = z
+
+                X = Xproj[0,:]
+                Y = Xproj[1,:]
+                Z = Xproj[2,:]
+                return [X,Y,Z]
+
+### backward compability
+def plotDataMainAxes(Xarray,PCaxes,h3,Npts,fname):
+        plotDataMainAxes(Xarray,fname, height=h3)
+
+def plotDataMainAxes(Xarray,fname, height=None):
+        if not len(Xarray)>0:
+                return False
+        Npts = len(Xarray[0])
+        if len(Xarray)>Npts:
+                PCaxes2 = getMainPCAaxes(Xarray)
+                [X1,X2,X3,S] = PCaxes2
+
+                ## Xarray to MxN numpy array
+                xx = np.zeros((Npts,len(Xarray)))
+                for i in range(0,len(Xarray)):
+                        for j in range(0,Npts):
+                                xx[j,i] = Xarray[i][j]
 
                 xx = preprocessing.scale(xx)
 
@@ -75,9 +98,64 @@ def plotDataMainAxes(Xarray,PCaxes,h3,Npts,fname):
                 #ax.set_ylim3d(-6, 6)
                 #ax.set_zlim3d(-6, 6)
 
-                ax.text(-4,-3,2, "height="+str(np.around(float(h3),3)), None)
-                #ax.text(-4,-5,2, "variability="+str(np.around(variability,2)), None)
-                #plt.show()
+                variability = np.sum(S[0:3])/float(np.sum(S[0:]))
+                print np.around(S,2)
+                print "data variability on 3 main axes:",variability
+
+                if height is not None:
+                        ax.text(-7,-6,3, "height="+str(np.around(float(height),3)), None)
+                ax.text(-7,-8,3, "variability="+str(np.around(variability,2)), None)
                 savefig( fname, dpi=gcf().dpi)
+                return True
+        else:
+                return False
 
 
+def plotDataMainAxesSurface(Xarray,fname, height=None):
+        if not len(Xarray)>0:
+                return False
+
+        Npts = len(Xarray[0])
+        if len(Xarray)>Npts:
+                PCaxes2 = getMainPCAaxes(Xarray)
+                [X1,X2,X3,S] = PCaxes2
+
+
+                ## Xarray to MxN numpy array
+                xx = np.zeros((Npts,len(Xarray)))
+                for i in range(0,len(Xarray)):
+                        for j in range(0,Npts):
+                                xx[j,i] = Xarray[i][j]
+
+                xx = preprocessing.scale(xx)
+
+                Xproj = np.zeros((3,len(Xarray)))
+                for i in range(0,len(Xarray)):
+                        x = np.dot(X1.T,xx[:,i])
+                        y = np.dot(X2.T,xx[:,i])
+                        z = np.dot(X3.T,xx[:,i])
+                        Xproj[0,i] = x
+                        Xproj[1,i] = y
+                        Xproj[2,i] = z
+
+                X = Xproj[0,:]
+                Y = Xproj[1,:]
+                Z = Xproj[2,:]
+
+                fig=figure(1)
+                fig.clf()
+                ax = fig.gca(projection='3d')
+                #ax.scatter(X,Y,Z,marker='o',c='r',s=5)
+                plot_trisurf_fixed(ax,X,Y,Z,cmap=cm.jet, linewidth=0,vmax=0.1)
+
+                #ax.set_xlim3d(-8, 8)
+                #ax.set_ylim3d(-6, 6)
+                #ax.set_zlim3d(-6, 6)
+
+                variability = np.sum(S[0:3])/float(np.sum(S[0:]))
+                print np.around(S,2)
+                print "data variability on 3 main axes:",variability
+
+                return True
+        else:
+                return False

mpp/robot/htoq.py
@@ -9,12 +9,10 @@ from mathtools.plotter import rotFromRPY
 import math
 import numpy as np
 
-DEBUG=0
-
 def htoq(k,h1,h2,h3):
 
         if k>3 or k<0:
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "[WARNING] k not in {0,1,2,3}",k
                 return [None,None]
 
@@ -27,12 +25,12 @@ def htoq(k,h1,h2,h3):
 
         l1 = h1 - dk
         if l1 < 0:
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "h1>dk",h1,dk
                 return [None,None]
         l2 = h3 - d2 - h1
         if l2 < 0:
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "h3 < d2-h1",h3,d2,h1
                 return [None,None]
 
@@ -42,11 +40,11 @@ def htoq(k,h1,h2,h3):
         sqrtA = 4*l1*l1*d0*d0-pow(l1*l1-d1*d1+d0*d0,2)
         sqrtB = 4*d5*d5*d3*d3-pow(d5*d5-d4*d4+d3*d3,2)
         if sqrtA < 0:
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "sqrtA<0"
                 return [None,None]
         if sqrtB < 0:
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "sqrtB<0",d5,d4,d3,l1,l2,h2
                 return [None,None]
 
@@ -55,7 +53,7 @@ def htoq(k,h1,h2,h3):
 
         l0 = sqrt(d0*d0 - a*a)
         if math.isnan(l0):
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "d0 < a",d0,a
                 return [None,None]
 
@@ -98,15 +96,15 @@ def htoq(k,h1,h2,h3):
 
         ###check assumption on injectivity
         inj_assumptions = \
-                [((v0+vk)[2] > vk[2]),
-                ((v0+v1)[2] > v0[2]),
-                ((v1+v2)[2] > v1[2]),
-                ((v2+v3)[2] > v2[2]),
-                ((v3+v4)[2] > v3[2])]
+                [((v0+vk)[2]>= vk[2]),
+                ((v0+v1)[2] >= v0[2]),
+                ((v1+v2)[2] >= v1[2]),
+                ((v2+v3)[2] >= v2[2]),
+                ((v3+v4)[2] >= v3[2])]
 
         if np.sum(inj_assumptions) < len(inj_assumptions):
                 ## trajectory not injective
-                if DEBUG:
+                if DEBUG_HTOQ:
                         print "path not injective!",vk[2],v0[2],v1[2],v2[2],v3[2],v4[2]
                 return [None,None]
 
@@ -126,7 +124,7 @@ def htoq(k,h1,h2,h3):
                 g1rel = acos(np.dot(v0n.T,v1n.T))
                 g2rel = acos(np.dot(v1n.T,v2n.T))
 
-                if DEBUG:
+                if DEBUG_HTOQ:
                         ### check assumption cases
                         vtest = np.dot(RYneg,v0)
                         if np.dot(v1n,vtest) < 0:
@@ -178,7 +176,7 @@ def htoq(k,h1,h2,h3):
                                 q[4] = g4
                         else:
                                 q[4] = -g4
-                        if DEBUG:
+                        if DEBUG_HTOQ:
                                 ### check assumption cases
                                 vtest = np.dot(RYneg,v3n)
                                 if np.dot(v4n,vtest) < 0:
@@ -206,7 +204,7 @@ def htoq(k,h1,h2,h3):
                                 q[4] = g4
                         else:
                                 q[4] = -g4
-                        if DEBUG:
+                        if DEBUG_HTOQ:
                                 ### check assumption cases
                                 vtest = np.dot(RYpos,v3pn)
                                 if np.dot(v4pn,vtest) < 0:
@@ -219,7 +217,7 @@ def htoq(k,h1,h2,h3):
                 g1rel = acos(np.dot(v0pn.T,v1pn.T))
                 g2rel = acos(np.dot(v1pn.T,v2n.T))
 
-                if DEBUG:
+                if DEBUG_HTOQ:
                         vtest = np.dot(RYpos,v0pn)
                         if np.dot(v1pn,vtest) < 0:
                                 print "Assumption that v1p lies on the left side of v0p is violated"
@@ -257,12 +255,13 @@ def htoq(k,h1,h2,h3):
 
                         ## due to the nature of circle intersections, v4 has to
                         ## lie on the right side of v3
-                        theta[4] = g4rel
+                        #theta[4] = g4rel
+                        theta[4] = 0.0
                         if v4n[0] > 0:
                                 q[4] = g4
                         else:
                                 q[4] = -g4
-                        if DEBUG:
+                        if DEBUG_HTOQ:
                                 ### check assumption cases
                                 vtest = np.dot(RYneg,v3n)
                                 if np.dot(v4n,vtest) < 0:
@@ -282,12 +281,13 @@ def htoq(k,h1,h2,h3):
                                 theta[3] = g3
 
                         ## v4 lies on the left side of v3
-                        theta[4] = -g4rel
+                        #theta[4] = -g4rel
+                        theta[4] = 0.0
                         if v4pn[0] > 0:
                                 q[4] = g4
                         else:
                                 q[4] = -g4
-                        if DEBUG:
+                        if DEBUG_HTOQ:
                                 ### check assumption cases
                                 vtest = np.dot(RYpos,v3pn)
                                 if np.dot(v4pn,vtest) < 0:

mpp/robot/robotspecifications.py
@@ -1,8 +1,16 @@
 from math import pi,cos,sin
 import numpy as np
+
+############################################################
+## DEBUG
+############################################################
+DEBUG_HTOQ = 0
+DEBUG_QTOX = 0
+DEBUG_HTOX = 0
 ############################################################
 ## ROBOT CONSTANTS
 ############################################################
+
 ROBOT_SPHERE_RADIUS = 0.3 #the max sphere embedded in the irreducible volume
 ROBOT_MAX_SLOPE = 5 # in degrees
 ROBOT_FOOT_RADIUS = 0.15 # in m
@@ -64,8 +72,11 @@ THETA_LIMITS_UPPER = np.array((0.73,2.62,0.73,1.05,0.79))
 THETA_SELFCOLLISION_LOWER = np.array((-1.2,0.0,-1.5,0.0,-0.2))
 THETA_SELFCOLLISION_UPPER = np.array((0.0,2.3,0.15,0.9,0.1))
 
-QLIMIT_HOMOTOPY01_LOWER = np.array((-1.2,0.0,-0.1,-0.8,-0.2))
-QLIMIT_HOMOTOPY01_UPPER = np.array(( 0.0,1.3, 0.1, 0.0, 0.2))
+#QLIMIT_HOMOTOPY01_LOWER = np.array((-1.2,0.0,-0.1,-0.8,-0.2))
+#QLIMIT_HOMOTOPY01_UPPER = np.array(( 0.0,1.3, 0.1, 0.0, 0.2))
+
+QLIMIT_HOMOTOPY01_LOWER = np.array(( 0.0,-1.3,-0.1, 0.0,-0.2))
+QLIMIT_HOMOTOPY01_UPPER = np.array(( 1.2, 0.0, 0.1, 0.8, 0.2))
 
 QLIMIT_HOMOTOPY23_LOWER = -QLIMIT_HOMOTOPY01_UPPER
 QLIMIT_HOMOTOPY23_UPPER = -QLIMIT_HOMOTOPY01_LOWER